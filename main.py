a49af3e719c79118125bb8e43e862de00ab7ef86
SchalkeFelix <felix.mehrhoff@rwth-aachen.de>
2024-04-29 17:32:55 +0200

    Gurobi installiert, Optimierung lüppt

--- main.py
+++ main.py
@@ -2,36 +2,29 @@
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import numpy as np
-from scipy.optimize import linprog
+import gurobipy as gp
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Create a new model
+    m = gp.Model()
 
+    # Create variables
+    x = m.addVar(vtype='B', name="x")
+    y = m.addVar(vtype='B', name="y")
+    z = m.addVar(vtype='B', name="z")
 
-    # Koeffizienten der Zielfunktion (Anzahl der Kabel minimieren)
-    c = [200, 100, 1000, 1000]  # Wir minimieren x1 + x2
+    # Set objective function
+    m.setObjective(x + y + 2 * z, gp.GRB.MAXIMIZE)
 
-    # Ungleichungsbedingungen (Leistungsgleichung: 100*x1 + 40*x2 >= 250)
-    A = [[-100, -50, -20, -10]]  # Koeffizienten der Ungleichung (linke Seite der Gleichung)
-    b = [-250]  # Rechte Seite der Ungleichung
+    # Add constraints
+    m.addConstr(x + 2 * y + 3 * z <= 4)
+    m.addConstr(x + y >= 1)
 
-    # Bereich für x1 und x2 (ganzzahlige Werte)
-    bounds = [(0, None), (0, None),(0, None), (0, None)]  # x1, x2 >= 0
+    # Solve it!
+    m.optimize()
 
-    # Optimierung durchführen
-    result = linprog(c, A_ub=A, b_ub=b, bounds=bounds, method='highs')
-
-    # Ergebnisse ausgeben
-    x1 = int(round(result.x[0]))  # Anzahl der 100 kW-Kabel
-    x2 = int(round(result.x[1]))  # Anzahl der 40 kW-Kabel
-    x3 = int(round(result.x[2]))  # Anzahl der 100 kW-Kabel
-    x4 = int(round(result.x[3]))  # Anzahl der 40 kW-Kabel
-
-    print("Optimale Anzahl der 100 kW-Kabel:", x1)
-    print("Optimale Anzahl der 50 kW-Kabel:", x2)
-    print("Optimale Anzahl der 20 kW-Kabel:", x3)
-    print("Optimale Anzahl der 10 kW-Kabel:", x4)
-    print("Gesamtanzahl der Kabel:", x1 + x2+ x3+x4)
+    print(f"Optimal objective value: {m.objVal}")
+    print(f"Solution values: x={x.X}, y={y.X}, z={z.X}")
